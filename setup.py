b80bbda60c7a57f0a1512f38f7532df2d98c4dab
Julie Cass <40177942+jcass11@users.noreply.github.com>
2019-04-30 10:13:19 -0700

    Fix concurrent.futures->futures in requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn', 'concurrent.futures']
+requirements = ['numpy', 'scipy', 'pandas', 'matplotlib', 'seaborn', 'futures']
 
 setup_requirements = ['pytest-runner', ]
 
