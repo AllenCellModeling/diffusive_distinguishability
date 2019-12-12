eec5931c1ddf82f2b1b4a1b29b97237b4a446667
Julie Cass <juliec@dev-aics-gjp-002.corp.alleninstitute.org>
2019-12-12 11:06:48 -0800

    Fix error magnitude bug

--- diffusive_distinguishability/ndim_homogeneous_distinguishability.py
+++ diffusive_distinguishability/ndim_homogeneous_distinguishability.py
@@ -326,7 +326,7 @@ def get_single_error(dim, d_const, n_steps, dt, n, loc_std, mag=None, hurst=None
     error = 100*((post_mean - d_const)/d_const)
     if mag is not None and mag:
         error = np.abs(error)
-    return 100*((post_mean - d_const)/d_const)
+    return error
 
 
 def get_dim_error(n_dim, d_const, n_steps, dt, n_reps, show_plot, loc_std=0, mag=None, hurst=None):
@@ -387,13 +387,11 @@ def error_sensitivity(d_const, n_steps_list, dt, n_reps, loc_std, mag=None, hurs
     data3 = np.zeros((len(n_steps_list), len(loc_std)))
     for n_steps in n_steps_list:
         for std in loc_std:
-            p_error = get_dim_error([1, 2, 3], d_const, n_steps, dt, n_reps, False, std, mag, hurst)
+            p_error = get_dim_error(n_dim=[1, 2, 3], d_const=d_const, n_steps=n_steps, dt=dt, n_reps=n_reps, show_plot=False, loc_std=std, mag=mag, hurst=hurst)
             ind_steps, ind_error = np.asarray(n_steps_list).searchsorted(n_steps), np.asarray(loc_std).searchsorted(std)
             data1[ind_steps, ind_error] = np.mean(p_error[0])
             data2[ind_steps, ind_error] = np.mean(p_error[1])
             data3[ind_steps, ind_error] = np.mean(p_error[2])
-            print('Error: '+str(std))
-        print('STEPS: '+str(n_steps)+'----------------------------------')
     df1 = pd.DataFrame(data=data1, columns=loc_std, index=n_steps_list)
     df2 = pd.DataFrame(data=data2, columns=loc_std, index=n_steps_list)
     df3 = pd.DataFrame(data=data3, columns=loc_std, index=n_steps_list)
